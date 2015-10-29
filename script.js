7fbe02a3754935de7b2f746f8a5aa67f989fd00d
Cliff Chen <cliffchen921@gmail.com>
2015-10-28 18:00:31 -0700

    slightly prettier version

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 /**
  * Created by Cliff on 10/28/2015.
  */
-// var operators = ['+', '-', 'x', '÷'];
 
+var operators = ['+', '-', 'x', '÷'];
+var keys = $('.calculator.keys');
+
+
+
+for (var i = 0; i<keys.length; i++){
+    keys[i].onclick=function(e){
+        var input=$('.screen');
+        var inputVal=input.innerhtml;
+        var btnVal=this.innterhtml;
+    }
+}
